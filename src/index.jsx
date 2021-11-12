4a331c7b3864cf5b70e90c59f1f416a00d1b0bff
poster834 <poster834@gmail.com>
2021-11-12 18:01:07 +0300

    12-11-21

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+window.addEventListener('load',() => {
+    ReactDOM.render(<header/>, document.getElementById("react_root"));
+})
