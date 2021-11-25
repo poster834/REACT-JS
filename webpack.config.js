0d3ab40b379ce0483466b21ca9e1479490a29a4d
poster834 <poster834@gmail.com>
2021-11-25 10:25:28 +0300

    25.11.2021 (error resolve)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
+const MODE_ENV = process.env.MODE_ENV;
 module.exports={
     entry:path.resolve(__dirname, 'src/index.jsx'),
     output:{
         path:path.resolve(__dirname, 'dist'),
         filename: 'index.js'
-    }
+    },
+    resolve:{
+        extensions:['js','jsx','tsx','ts','json']
+    },
+    mode: MODE_ENV ? MODE_ENV : 'development',
+    
+   
+    module: {
+        rules: [
+            { test:/\.[tj]sx?$/,use:['ts-loader'] },
+            
+        ],
+    },
 };
