f3df6b7e76058d03a2231ffc671cd6815be51a8f
poster834 <poster834@gmail.com>
2021-12-17 09:49:54 +0300

    err DevTool

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,16 +1,28 @@
 const path = require('path');
+
+const {HotModuleReplacementPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
 module.exports={
-    resolve:{
-        extensions: ['.js', '.jsx','.ts', '.tsx','.json']
-    },
     mode: NODE_ENV ? NODE_ENV:'development',
-    entry:path.resolve(__dirname, '../src/client/index.jsx'),
+    resolve:{
+        extensions: ['.js', '.jsx','.ts', '.tsx','.json'],
+        alias: {
+            'react-dom':IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+        }
+    },
+    
+    entry:[
+        path.resolve(__dirname, '../src/client/index.jsx'),
+        'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+    ],
     output:{
         path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js'
+        filename: 'client.js',
+        publicPath: '/static/',
     },
     module: {
         rules: [{
@@ -18,5 +30,12 @@ module.exports={
             use:['ts-loader']
             }]
     },
+    devtool: setupDevtool(),
+    plugins: IS_DEV ? [
+        new CleanWebpackPlugin(),
+        new HotModuleReplacementPlugin(),
+    ]
+    : 
+    [],
 
 };
