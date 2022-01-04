b22a794065c7f34a12317660f018d9b6952e3136
poster834 <poster834@gmail.com>
2022-01-04 12:55:23 +0300

    homeWork-2

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,10 +1,11 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from "react";
+import styles from './header.css';
 
 function HeaderComponent(){
     return(
         <header>
-            <h2>Hello, React!</h2>
+            <h2 className={styles.example}>Hello, React!</h2>
         </header>
     );
 } 
